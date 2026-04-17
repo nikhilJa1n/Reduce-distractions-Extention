56a499e9c08e20e6a3d0af6abde4d05c1747f795
Nikhil Jain <nikhil@email.com>
2026-04-17 06:54:53 +0530

    chore: add whitespace for readability in newtab.js

--- newtab.js
+++ newtab.js
@@ -17,6 +17,7 @@ const QUOTES = [
     { text: "The best time to plant a tree was 20 years ago. The second best time is now.", author: "Chinese Proverb" },
 ];
 
+
 const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
